0d5c1aa41faa6efe39b9144e85d98e6b11e15467
Ramakrishna G Kannari <92996109+ramkrishnareddy24@users.noreply.github.com>
2023-08-26 15:04:08 +0530

    first

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import authRoutes from "./routes/authRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js"
 import cors from "cors";
+import path from "path";
 
 //configure env
 dotenv.config();
@@ -21,6 +22,7 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname,'./client/build')))
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -28,8 +30,8 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 //rest api
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to APP</h1>");
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 //PORT
